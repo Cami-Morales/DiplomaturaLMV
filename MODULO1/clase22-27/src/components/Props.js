7ee77a520eba0a3a2bf92c0a52f9928f24086c8d
Camila Morales <camimoralesyt@gmail.com>
2023-06-25 12:23:26 -0300

    Clase 24

--- MODULO1/clase22-27/src/components/Props.js
+++ MODULO1/clase22-27/src/components/Props.js
@@ -1,9 +1,64 @@
-const EjemploProps01 = props =>{
-    return(
+const EjemploProps01 = props => {
+    return (
         <h3>Hola {props.nombre}</h3>
     )
 }
 
+
+const EjemploProps02 = props => {
+
+    const { elementos } = props;
+
+    return (
+        <ul>
+            {elementos.map(elemento => <li key={elemento}> {elemento} </li>)}
+        </ul>
+    )
+
+
+}
+
+const EjemploProps03 = ({titulo,subtitulo,cuerpo}) => {
+    return(
+        <div>
+            <h4>{titulo}</h4>
+            <h5>{subtitulo}</h5>
+            <p>{cuerpo}</p>
+        </div>
+    )
+}
+
+const EjemploProps04 = props =>{
+    // cuando haga el evento onChange del input
+    // va a ejecutar la función anónima
+    // que lo que hace es cambiarValor (recibimos de la propiedad)> mostrando las letras
+    //cambiarValor > va a estar creado en Ejemplo1
+    return(
+        <label>ver en consola: 
+            <input type="text" onChange={(e) => 
+            props.cambiarVarlor(e.target.value)
+        } /></label>
+    )
+}
+
+const EjemploProps05 = props =>{
+
+    const handerClick= e =>{
+        if(props.eventoClick){
+            props.eventoClick('me cliqueaste')
+        }
+    }
+
+    return(
+        <p><button onClick={handerClick}>Cliqueame!</button></p>
+    )
+}
+
+
 export {
-    EjemploProps01
+    EjemploProps01,
+    EjemploProps02,
+    EjemploProps03,
+    EjemploProps04,
+    EjemploProps05
 }
--- MODULO1/clase22-27/src/components/layout/pages/Ejemplo1.js
+++ MODULO1/clase22-27/src/components/layout/pages/Ejemplo1.js
@@ -1,9 +1,30 @@
-import { EjemploProps01 } from "../../Props";
+import { EjemploProps01, EjemploProps02, EjemploProps03, EjemploProps04, EjemploProps05 } from "../../Props";
+
+const name = 'Pablo'
+
+const lista =['manzanas', 'bananas', 'peras', 'paltas']
+
+const mostrarValor = valor =>{
+    console.log(valor)
+}
+
 
 const Ejemplo1 = (props) => {
     return(
         <div>
         <h1>Ejemplos de propiedades</h1>
+        {/* propiedad simple y reutilizable */}
+        <EjemploProps01 nombre='Camila' />
+        <EjemploProps01 nombre={name} />
+        {/* listado de elementos */}
+        <EjemploProps02 elementos={lista} />
+        {/* multiples propiedades y destructuring */}
+        <EjemploProps03 titulo='noticia x' subtitulo='aca estamos aprendiendo react' cuerpo='soy el cuerpo de la noticia' />
+        {/* Evento - al tener Cambiar Valor funciona*/}
+        <EjemploProps04 cambiarValor={mostrarValor} />
+        {/* Evento - tenemos con if que controla antes */}
+        <EjemploProps05 eventoClick={mostrarValor}/> 
+
         </div>
     )
 }
